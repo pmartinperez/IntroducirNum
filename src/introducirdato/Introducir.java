c7db2d96e4bc68c2921e49f2d4414bfea6cb9f48
Patricia <Patricia@Patri>
2015-02-19 09:11:07 +0100

    hecho el javadoc

--- src/introducirdato/Introducir.java
+++ src/introducirdato/Introducir.java
@@ -3,12 +3,21 @@ package introducirdato;
 
 import java.util.Scanner;
 import javax.swing.JOptionPane;
+/**
+ * Clase con un metodo para introducir numero
+ * @author Patricia
+ * @version 1.0
+ */
 
 
 public class Introducir {
-    //metodo para introducir numero por teclado
+    /**
+     * Metodo para introducir numero
+     * @param num si num=0 introduces por teclado, si num=1 por pantalla
+     * @return devuelve el numero introducido
+     */
     public int introducirDato(int num){
-               Scanner dato=new Scanner(System.in);
+        Scanner dato=new Scanner(System.in);
         int op=0;
                
         switch(num){
